f279fe17485ce091ae55b6585ba2a232c9fc1e66
Zadkiel <zadkiel679@gmail.com>
2024-05-10 00:28:00 -0400

    Created the base layout of the project

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BaseLayoutComponent } from './base-layout/base-layout.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BaseLayoutComponent,
+    AuthLayoutComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    SignInComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
